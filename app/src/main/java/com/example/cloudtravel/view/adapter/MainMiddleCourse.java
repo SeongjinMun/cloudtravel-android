874bdf514677795cc77e2bc63d23ea8d3cfe7743
ziaso <105402883+ziaso@users.noreply.github.com>
2022-06-06 21:26:39 +0900

    FEAT : Add MainHomeCourse RecyclerView

---
+++ app/src/main/java/com/example/cloudtravel/view/adapter/MainMiddleCourse.java
@@ -0,0 +1,68 @@
+package com.example.cloudtravel.view.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cloudtravel.R;
+import com.example.cloudtravel.model.RecyclerViewItem;
+
+import java.util.ArrayList;
+
+public class MainMiddleCourse extends RecyclerView.Adapter<MainMiddleCourse.ViewHolder> {
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgView_item;
+        TextView txt_main;
+        TextView txt_sub;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imgView_item = (ImageView) itemView.findViewById(R.id.imgView_item);
+            txt_main = (TextView) itemView.findViewById(R.id.txt_main);
+            txt_sub = (TextView) itemView.findViewById(R.id.txt_sub);
+        }
+    }
+
+    private ArrayList<RecyclerViewItem> mList = null;
+
+    public MainMiddleCourse(ArrayList<RecyclerViewItem> mList) {
+        this.mList = mList;
+    }
+
+    // 아이템 뷰를 위한 뷰홀더 객체를 생성하여 리턴
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = inflater.inflate(R.layout.activity_recycler_item, parent, false);
+        MainMiddleCourse.ViewHolder vh = new MainMiddleCourse.ViewHolder(view);
+        return vh;
+    }
+
+    // position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시
+    @Override
+    public void onBindViewHolder(@NonNull MainMiddleCourse.ViewHolder holder, int position) {
+        RecyclerViewItem item = mList.get(position);
+
+        holder.imgView_item.setImageResource(R.drawable.ic_baseline_cloud_24);
+        holder.txt_main.setText(item.getMainText());
+        holder.txt_sub.setText(item.getSubText());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+
+}
--- app/src/main/java/com/example/cloudtravel/view/fragment/HomeFragment.java
+++ app/src/main/java/com/example/cloudtravel/view/fragment/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.example.cloudtravel.view.fragment;
 
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 import java.util.ArrayList;
 
@@ -16,13 +18,23 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.cloudtravel.R;
 import com.example.cloudtravel.databinding.FragmentHomeBinding;
+import com.example.cloudtravel.model.RecyclerViewItem;
 import com.example.cloudtravel.view.adapter.MainBannerAdapter;
+import com.example.cloudtravel.view.adapter.MainMiddleCourse;
+import com.example.cloudtravel.view.adapter.MainTopCourse;
 import com.example.cloudtravel.viewModel.MainViewModel;
 
 public class HomeFragment extends Fragment {
     private ViewPager2 viewPager2;
     private MainViewModel viewModel;
     private FragmentHomeBinding binding;
+    private ArrayList<RecyclerViewItem> mList;
+    private RecyclerView MainTopRecyclerView;
+    private MainTopCourse MainTopRecyclerViewAdapter;
+    private RecyclerView MainMiddleRecyclerView;
+    private MainMiddleCourse MainMiddleRecyclerViewAdapter;
+    private RecyclerView MainBottomRecyclerView;
+    private MainMiddleCourse MainBottomRecyclerViewAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -63,9 +75,58 @@ public class HomeFragment extends Fragment {
         viewPager2 = getView().findViewById(R.id.bannerList);
         viewPager2.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
         viewPager2.setAdapter(adapter);
+
+        MainTopCourse();
+        MainMiddleCourse();
+        MainBottomCourse();
+
+        for(int i=0;i<5;i++){
+            addItem("iconName", "Taek" + i, "taek2.tistory.com");
+        }
+
+        MainTopRecyclerViewAdapter = new MainTopCourse(mList);
+        MainTopRecyclerView.setAdapter(MainTopRecyclerViewAdapter);
+        MainTopRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        MainTopRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+
+        MainMiddleRecyclerViewAdapter = new MainMiddleCourse(mList);
+        MainMiddleRecyclerView.setAdapter(MainMiddleRecyclerViewAdapter);
+        MainMiddleRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        MainMiddleRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+
+        MainBottomRecyclerViewAdapter = new MainMiddleCourse(mList);
+        MainBottomRecyclerView.setAdapter(MainBottomRecyclerViewAdapter);
+        MainBottomRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        MainBottomRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+
     }
 
     private void setContentView(int fragment_home) {
+    }
+
+    public void MainTopCourse(){
+        MainTopRecyclerView = (RecyclerView) getView().findViewById(R.id.MainTopCourse);
+        mList = new ArrayList<>();
+    }
+
+    public void MainMiddleCourse(){
+        MainMiddleRecyclerView = (RecyclerView) getView().findViewById(R.id.MainMiddleCourse);
+        mList = new ArrayList<>();
+    }
+
+    public void MainBottomCourse(){
+        MainBottomRecyclerView = (RecyclerView) getView().findViewById(R.id.MainBottomCourse);
+        mList = new ArrayList<>();
+    }
+
+    public void addItem(String imgName, String mainText, String subText){
+        RecyclerViewItem item = new RecyclerViewItem();
+
+        item.setImgName(imgName);
+        item.setMainText(mainText);
+        item.setSubText(subText);
+
+        mList.add(item);
     }
 
 }
